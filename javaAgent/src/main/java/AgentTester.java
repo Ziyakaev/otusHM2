6155408d360becc4bbdb2cf4e0e31cedba3ab795
Artur <vip.ziyakaev@gmail.com>
2018-04-29 18:51:16 +0300

    add agent project

---
+++ javaAgent/src/main/java/AgentTester.java
@@ -0,0 +1,7 @@
+import java.lang.instrument.Instrumentation;
+
+public class AgentTester{
+    public static void main(String[] args) {
+        System.out.println("Hello! i am agent Tester");
+    }
+}
---
+++ javaAgent/src/main/java/agent/Agent007.java
@@ -0,0 +1,7 @@
+package agent;
+
+public class Agent007 {
+    public static void premain(String args) {
+        System.out.println("Hello! I`m java agent");
+    }
+}
